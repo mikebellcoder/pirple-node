f46e3ff46a4cba133970d8c5ca1035cc98682b62
mikebell <mikebell88008@gmail.com>
2020-06-01 19:38:35 -0700

    Finished /tokens lesson

--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -31,6 +31,31 @@ helpers.parseJsonToObject = function(str) {
     }
 }
 
+// Create a string of random alphanumeric characters, of a given length
+helpers.createRandomString = function(strLength) {
+    strLength = typeof strLength == 'number' && strLength > 0
+    ? strLength
+    : false;
+
+    if (strLength) {
+        // Define all the possible characters that could go into a string
+        const possibleCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+        // Start the final string
+        let str = '';
+
+        for (i = 1; i <= strLength; i++) {
+            // Get a random character from the possibleCharacters string
+            let randomCharacter = possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+            // Append this character to the final string
+            str += randomCharacter;
+        }
+        // Return random string
+        return str;
+    } else {
+        return false;
+    }
+}
+
 
 
 
